dfa9ac648f802b97e7b45d808a253c545b08cee6
CESAR CAÑAS CASELLA <cesarbmx@hotmail.com>
2020-05-09 07:30:47 -0400

    Initial commit

---
+++ RenameSubFolderAndFiles/Services/ArguementsConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using RenameSubFolderAndFiles.Models;
+
+namespace RenameSubFolderAndFiles.Services
+{
+    public class ArguementsConverter
+    {
+        public ArguementsConverter(string[] args)
+        {
+            args = new[] { @"C:\cesarc\GitHub\Shared", "Pinnacle.CustomerTeam", "CesarBmx" };
+
+            if (args.Length != 3)
+                throw new ArgumentException("3 args need to be supplied");
+
+            ArguementsModel = new ArguementsModel
+            {
+                Path = args[0],
+                ChangeFrom = args[1],
+                ChangeTo = args[2]
+            };
+
+            if(!Directory.Exists(ArguementsModel.Path))
+                throw new ArgumentException("Path does not exist.");
+        }
+
+        public ArguementsModel ArguementsModel { get; }
+    }
+}
